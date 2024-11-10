914a3ea2b2b38fbadbfa85e399dd082cb68ae2c5
Alex <zakrevskyalex1323@gmail.com>
2024-11-10 16:09:58 +0300

    (test) Tested basic auth logic

--- finance_project/finance_app/finance_api/tests/test_auth.py
+++ finance_project/finance_app/finance_api/tests/test_auth.py
@@ -3,9 +3,53 @@ from rest_framework import status
 from django.test import TestCase
 
 
-class UserViewsAPITest(TestCase):
+class LoginAPITest(TestCase):
     def setUp(self):
         self.client = APIClient()
+        self.login_url = "/api/auth/login"
+        self.register_url = "/api/auth/register"
+
+        self.username = "test_user"
+        self.password = "12334fhf_edhD"
+        self.test_email = "test_email@gmail.com"
+
+        self.test_user = self.client.post(
+            self.register_url,
+            {
+                "username": self.username,
+                "email": self.test_email,
+                "password": self.password,
+            },
+        )
+
+    def login_user(self):
+        response = self.client.post(
+            self.login_url, {"username": self.username, "password": self.password}
+        )
+        return {"access": response["access"], "refresh": response["refresh"]}
+
+    
+    
+    def test_valid_user_login(self):
+        response = self.client.post(
+            self.login_url, {"username": self.username, "password": self.password}
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertContains(response, "access")
+        self.assertContains(response, "refresh")
+
+    def test_invalid_credentials_rejected(self):
+        response = self.client.post(
+            self.login_url, {"username": "not_exists", "password": "not_exists"}
+        )
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class RegistrarionAPITest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.register_url = "/api/auth/register"
+
         self.test_email_1 = "test_email_1@gmail.com"
         self.test_email_2 = "test_email_2@gmail.com"
         self.test_user_name_1 = "test_username_1"
@@ -15,7 +59,7 @@ class UserViewsAPITest(TestCase):
     def test_valid_user_registration(self):
         "Valid test case"
         response = self.client.post(
-            "/api/users/register",
+            self.register_url,
             {
                 "username": self.test_user_name_1,
                 "email": self.test_email_1,
@@ -27,7 +71,7 @@ class UserViewsAPITest(TestCase):
     def test_register_unique_email(self):
         "Email must be unique"
         response = self.client.post(
-            "/api/users/register",
+            self.register_url,
             {
                 "username": self.test_user_name_1,
                 "email": self.test_email_1,
@@ -35,7 +79,7 @@ class UserViewsAPITest(TestCase):
             },
         )
         response = self.client.post(
-            "/api/users/register",
+            self.register_url,
             {
                 "username": self.test_user_name_2,
                 "email": self.test_email_1,
@@ -47,7 +91,7 @@ class UserViewsAPITest(TestCase):
     def test_register_unique_username(self):
         "Username must be unique"
         response = self.client.post(
-            "/api/users/register",
+            self.register_url,
             {
                 "username": self.test_user_name_1,
                 "email": self.test_email_1,
@@ -55,7 +99,7 @@ class UserViewsAPITest(TestCase):
             },
         )
         response = self.client.post(
-            "/api/users/register",
+            self.register_url,
             {
                 "username": self.test_user_name_1,
                 "email": self.test_email_2,
