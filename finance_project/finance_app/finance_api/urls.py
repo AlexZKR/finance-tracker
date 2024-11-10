324228bd75e451790a29b31f92e2d837bf507f08
Alex <zakrevskyalex1323@gmail.com>
2024-11-10 15:29:41 +0300

    (feat) Basic JWT login logout

--- finance_project/finance_app/finance_api/urls.py
+++ finance_project/finance_app/finance_api/urls.py
@@ -6,14 +6,41 @@ from .views import (
     AccountViewSet,
     TransactionViewSet,
     UserCategoryViewSet,
+    CustomLoginView,
 )
+from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+    TokenVerifyView,
+)
+from rest_framework.schemas import get_schema_view
+
 
 router = DefaultRouter(trailing_slash=False)
 router.register(r"users", UserViewSet, basename="user")
-router.register(r"accounts", AccountViewSet)
+router.register(r"accounts", AccountViewSet, basename="account")
 router.register(r"transactions", TransactionViewSet)
 router.register(r"user_categories", UserCategoryViewSet)
 router.register(r"base_categories", BaseCategoryViewSet)
 
 
-urlpatterns = [path("", include(router.urls))]
+urlpatterns = [
+    path("", include(router.urls)),
+    path("auth/login", CustomLoginView.as_view(), name="login"),
+    path(
+        "auth/register",
+        UserViewSet.as_view({"post": "register"}, permission_classes=[AllowAny]),
+        name="register",
+    ),
+    path("token/refresh", TokenRefreshView.as_view(), name="token_refresh"),
+    path("token/verify", TokenVerifyView.as_view(), name="token_verify"),
+    path(
+        "openapi",
+        get_schema_view(
+            title="Personal finance tracker app",
+            description="Finance tracker API",
+            version="1.0.0",
+        ),
+        name="openapi-schema",
+    ),
+]
--- finance_project/finance_app/finance_api/views.py
+++ finance_project/finance_app/finance_api/views.py
@@ -11,12 +11,24 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import action
 
+from rest_framework_simplejwt.tokens import RefreshToken
+from .permissions import IsOwnerOrAdmin
+from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+
+class CustomLoginView(TokenObtainPairView):
+    permission_classes = [AllowAny]
+
 
 class UserViewSet(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
-    @action(detail=False, methods=["post"], url_path="register")
+    @action(
+        detail=False,
+        methods=["post"],
+    )
     def register(self, request):
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
@@ -33,10 +45,26 @@ class UserViewSet(ModelViewSet):
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    @action(detail=True, methods=["post"], url_path="logout")
+    def logout(self, request):
+        try:
+            refresh_token = RefreshToken(request.data["refresh"])
+            refresh_token.blacklist()
+            return Response(
+                data="User logout successful", status=status.HTTP_204_NO_CONTENT
+            )
+        except Exception as e:
+            return Response(data=e.__str__(), status=status.HTTP_400_BAD_REQUEST)
+
 
 class AccountViewSet(ModelViewSet):
     serializer_class = AccountSerializer
-    queryset = Account.objects.all()
+    permission_classes = [IsOwnerOrAdmin]
+
+    def get_queryset(self):
+        if self.request.user.is_staff:
+            return Account.objects.all()
+        return Account.objects.filter(user=self.request.user)
 
 
 class TransactionViewSet(ModelViewSet):
