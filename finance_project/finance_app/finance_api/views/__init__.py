bba4bf216134c6edb7d87df380886c750957534c
Alex <zakrevskyalex1323@gmail.com>
2024-11-13 18:12:42 +0300

    (tweak) moved views to separate module

---
+++ finance_project/finance_app/finance_api/views/__init__.py
@@ -0,0 +1,7 @@
+from .auth_views import LoginView, LogoutView, RefreshView, UserViewSet  # noqa: F401
+from .model_views import (  # noqa: F401
+    AccountViewSet,
+    BaseCategoryViewSet,
+    UserCategoryViewSet,
+    TransactionViewSet,
+)
--- finance_project/finance_app/finance_api/views/auth_views.py
+++ finance_project/finance_app/finance_api/views/auth_views.py
@@ -1,23 +1,16 @@
 import logging
-from .models import Account, BaseCategory, Transaction, User, UserCategory
-from .serializers import (
-    AccountSerializer,
-    BaseCategorySerializer,
-    TransactionSerializer,
-    UserSerializer,
-    UserCategorySerializer,
-)
-from rest_framework.viewsets import ModelViewSet
+
+from ..serializers import UserSerializer
+from ..models import User
+from ..auth import JWTBlackList, RedisJWTAuthentication
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import action
-from rest_framework.exceptions import PermissionDenied
-
-from .auth import IsOwnerOrAdmin
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .auth import JWTBlackList, RedisJWTAuthentication
+from rest_framework.viewsets import ModelViewSet
 
 logger = logging.getLogger("__name__")
 
@@ -85,30 +78,3 @@ class UserViewSet(ModelViewSet):
             )
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class AccountViewSet(ModelViewSet):
-    serializer_class = AccountSerializer
-    permission_classes = [IsOwnerOrAdmin]
-
-    def get_queryset(self):
-        if not self.request.user.is_authenticated:
-            raise PermissionDenied("You must be logged in to access this resource.")
-        if self.request.user.is_staff:
-            return Account.objects.all()
-        return Account.objects.filter(user=self.request.user)
-
-
-class TransactionViewSet(ModelViewSet):
-    queryset = Transaction.objects.all()
-    serializer_class = TransactionSerializer
-
-
-class UserCategoryViewSet(ModelViewSet):
-    queryset = UserCategory.objects.all()
-    serializer_class = UserCategorySerializer
-
-
-class BaseCategoryViewSet(ModelViewSet):
-    queryset = BaseCategory.objects.all()
-    serializer_class = BaseCategorySerializer
